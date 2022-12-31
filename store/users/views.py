f5f62593cfe8406f774baa7f820f388e6dd8e150
ReddNev <creedlll@mail.ru>
2022-12-31 12:47:37 +0300

    update

--- store/users/views.py
+++ store/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'users/login.html')
+
+
+def registration(request):
+    return render(request, 'users/registration.html')
