349c72a30688ff50bbec9fc5e49bfa82e69254ab
ReddNev <creedlll@mail.ru>
2022-12-31 13:13:21 +0300

    [ADD] form

--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from users.models import User
+from users.forms import UserLoginForm
+
 
 def login(request):
-    return render(request, 'users/login.html')
+    content = {'form': UserLoginForm()}
+    return render(request, 'users/login.html', content)
 
 
 def registration(request):
