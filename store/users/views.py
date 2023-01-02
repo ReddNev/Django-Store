c6400efea221867fda6c192665dd905cdcdc0506
ReddNev <creedlll@mail.ru>
2023-01-02 14:01:29 +0300

    [ADD] logout

--- store/users/views.py
+++ store/users/views.py
@@ -47,3 +47,8 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
     context = {'title': 'Store - Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
