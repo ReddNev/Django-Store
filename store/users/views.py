9b566301c7bc7514464d001b19e926450b2ef233
ReddNev <creedlll@mail.ru>
2023-01-02 13:36:02 +0300

    [ADD] error processing

--- store/users/views.py
+++ store/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.models import User
@@ -27,6 +27,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Поздравляем! Вы успешно зарегестрировались!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
