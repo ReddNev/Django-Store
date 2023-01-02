5d617dffb01bed78d2aadd73e8333ac792dab0e0
ReddNev <creedlll@mail.ru>
2023-01-02 14:01:10 +0300

    update

--- store/users/urls.py
+++ store/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from users.views import login, registration, profile
+from users.views import *
 
 app_name = 'users'
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', registration, name='registration'),
     path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout'),
+
 ]
