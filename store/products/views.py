c71f2c536e73814d1ca445b90baead8426c1c559
ReddNev <creedlll@mail.ru>
2023-01-04 15:17:32 +0300

    [ADD] and [REMOVE] items from the cart

--- store/products/views.py
+++ store/products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
+from users.models import User
 
 
 def index(request):
@@ -17,3 +18,23 @@ def products(request):
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.ssve()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
