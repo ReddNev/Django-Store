c47328c1569f82f3c522cd0b9ea7001e106a1e1c
ReddNev <creedlll@mail.ru>
2023-01-04 15:11:10 +0300

    [ADD] basket model

--- store/products/models.py
+++ store/products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128)
@@ -19,3 +21,13 @@ class Product(models.Model):
 
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category}'
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
