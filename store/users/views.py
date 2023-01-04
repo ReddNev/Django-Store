ca6225d34ba778e16d0c8eb79443ded3aa1b1e40
ReddNev <creedlll@mail.ru>
2023-01-04 17:10:13 +0300

    [ADD] authentication system

--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
@@ -36,6 +36,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
