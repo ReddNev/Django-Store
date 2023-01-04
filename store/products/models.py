64b73609809ce4a4a6ccd501f4e6de3b1feb5acc
ReddNev <creedlll@mail.ru>
2023-01-04 16:54:37 +0300

    [ADD] sum of goods in the basket

--- store/products/models.py
+++ store/products/models.py
@@ -23,11 +23,24 @@ class Product(models.Model):
         return f'Продукт: {self.name} | Категория: {self.category}'
 
 
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.amount() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
 class Basket(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
+    objects = BasketQuerySet.as_manager()
+
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
+
+    def amount(self):
+        return self.product.price * self.quantity
