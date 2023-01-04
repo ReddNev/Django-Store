7ce84d73f8af14b3c429c19dfe97a5dbc02a96ff
ReddNev <creedlll@mail.ru>
2023-01-04 17:10:44 +0300

    [ADD] authentication system

--- store/products/views.py
+++ store/products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from products.models import Product, ProductCategory, Basket
-from users.models import User
 
 
 def index(request):
@@ -20,6 +20,7 @@ def products(request):
     return render(request, 'products/products.html', context)
 
 
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -34,6 +35,7 @@ def basket_add(request, product_id):
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
+@login_required
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
