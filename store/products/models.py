6e6e22ed59d587e649ce110e8e98f9303d18b00a
ReddNev <creedlll@mail.ru>
2023-01-05 13:46:59 +0300

    [ADD] admin panel customization

--- store/products/models.py
+++ store/products/models.py
@@ -7,6 +7,10 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'категорию'
+        verbose_name_plural = 'категория'
+
     def __str__(self):
         return self.name
 
@@ -18,6 +22,10 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
 
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category}'
