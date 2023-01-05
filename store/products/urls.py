dafb913f54d404a2e41d3a2afc91edb6e9833521
ReddNev <creedlll@mail.ru>
2023-01-05 12:44:00 +0300

    [ADD] pagination

--- store/products/urls.py
+++ store/products/urls.py
@@ -7,6 +7,7 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='index'),
     path('category/<int:category_id>/', products, name='category'), # ../products/category/<id>/
+    path('page/<int:page_number>/', products, name='paginator'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
