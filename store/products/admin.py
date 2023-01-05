8e6d02e0b20e340445b4747a6d1b3c1bb0bfc35a
ReddNev <creedlll@mail.ru>
2023-01-05 13:47:16 +0300

    [ADD] admin panel customization

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Basket
 
-admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity')
+    extra = 0
