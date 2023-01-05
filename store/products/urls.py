370d9721b9e8cb742803bbba6f4b65aa80de99eb
ReddNev <creedlll@mail.ru>
2023-01-05 11:44:42 +0300

    [ADD] filtring by category in url

--- store/products/urls.py
+++ store/products/urls.py
@@ -6,6 +6,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
+    path('category/<int:category_id>/', products, name='category'), # ../products/category/<id>/
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
