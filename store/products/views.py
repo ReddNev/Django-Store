b3263296f6f5199920feb557d924a62b81a6acc5
ReddNev <creedlll@mail.ru>
2023-01-06 13:53:38 +0300

    update

--- store/products/views.py
+++ store/products/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
-
 from products.models import Product, ProductCategory, Basket
 
 
