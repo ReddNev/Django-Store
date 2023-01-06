4ca00ab431e9fe66852bd07dc98b87bb45cb4b79
ReddNev <creedlll@mail.ru>
2023-01-06 13:39:06 +0300

    [ADD] generic

--- store/products/urls.py
+++ store/products/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from products.views import products, basket_add, basket_remove
+from products.views import ProductsListView, basket_add, basket_remove
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index'),
-    path('category/<int:category_id>/', products, name='category'), # ../products/category/<id>/
-    path('page/<int:page_number>/', products, name='paginator'),
+    path('', ProductsListView.as_view(), name='index'),
+    path('category/<int:category_id>/', ProductsListView.as_view(), name='category'), # ../products/category/<id>/
+    path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
