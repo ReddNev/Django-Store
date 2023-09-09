ed274a8c332bef79b0bf70b3af9d1e924b54bb66
ReddNev <creedlll@mail.ru>
2023-09-09 15:57:55 +0300

    update

--- store/store/settings.py
+++ store/store/settings.py
@@ -80,6 +80,16 @@ INTERNAL_IPS = [
     'localhost',
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
